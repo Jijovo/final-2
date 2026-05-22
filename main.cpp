7694278b5700f27e8cc44138162cc3eab7b34be7
Jijovo <jijovo77@gmail.com>
2026-05-21 17:55:11 -0700

    milestone 2

--- main.cpp
+++ main.cpp
@@ -24,20 +24,23 @@ int main()
     head->name = names[rand() % 8];
     head->order = drinks[rand() % 5];
     //create randomised linked list with 3 customers and their orders
-    coffeeOrder* current = head;
+    coffeeOrder* tail = head;
     for (int i = 0; i < 3; i++)
     {
         coffeeOrder* newNode = new coffeeOrder;
         newNode->name = names[rand() % 8];
         newNode->order = drinks[rand() % 5];
         newNode->next = nullptr;
-        current->next = newNode;
-        current = newNode;
+        tail->next = newNode;
+        tail = newNode;
     }
 
     //run simulation, 10 rounds, 50% of customer joining each round, first in line is always served
     for (int i = 0; i < 10; i++)
     {
+        //display round
+        cout << endl << "Round " << i + 1 << ": " <<endl;
+        
         //if customer joins
         if (rand() % 100 < 50)
         {
@@ -46,27 +49,43 @@ int main()
             newNode->name = names[rand() % 8];
             newNode->order = drinks[rand() % 5];
             newNode->next = nullptr;
-            current->next = newNode;
-            current = newNode;
+            tail->next = newNode;
+            tail = newNode;
             //print
             cout << newNode->name << " joined the queue." << endl;
         }
 
-        //serve head
-        coffeeOrder* temp = head;
-        head = head->next;
-        cout << temp->name << " ordered: " << temp->order << endl;
-        delete temp;
-
-        //print queue
-        cout << "Queue: ";
-        current = head;
-        while (current != nullptr)
+        //check if queue if empty
+        if (head->next == nullptr)
         {
-            cout << current->name << " ";
-            current = current->next;
+            cout << "The queue is empty." << endl;
         }
-        cout << endl;
+        else {
+            //serve head
+            coffeeOrder* temp = head;
+            head = head->next;
+            cout << temp->name << " is served." << endl;
+            delete temp;
+
+            //print remaining queue
+            coffeeOrder* current = head;
+            cout << endl << "Queue:" << endl;
+            while (current != nullptr)
+            {
+                cout << current->name << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    //delete linked list
+    coffeeOrder* currentd = head;
+    while (currentd != nullptr)
+    {
+        coffeeOrder* temp = currentd;
+        currentd = currentd->next;
+        delete temp;
     }
 
     return 0;
