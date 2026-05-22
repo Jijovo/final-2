059557ea91bfb5424717a2526a81d23b8e290215
Jijovo <jijovo77@gmail.com>
2026-05-21 17:48:53 -0700

    update 2

--- main.cpp
+++ main.cpp
@@ -19,23 +19,55 @@ int main()
     //set random seed
     srand(time(0));
 
-    //create randomised linked list
-    coffeeOrder* head = nullptr;
-    for (int i = 0; i < 8; i++)
+    //create head node
+    coffeeOrder* head = new coffeeOrder;
+    head->name = names[rand() % 8];
+    head->order = drinks[rand() % 5];
+    //create randomised linked list with 3 customers and their orders
+    coffeeOrder* current = head;
+    for (int i = 0; i < 3; i++)
     {
         coffeeOrder* newNode = new coffeeOrder;
         newNode->name = names[rand() % 8];
         newNode->order = drinks[rand() % 5];
-        newNode->next = head;
-        head = newNode;
+        newNode->next = nullptr;
+        current->next = newNode;
+        current = newNode;
     }
 
-    //print the linked list
-    coffeeOrder* current = head;
-    while (current != nullptr)
+    //run simulation, 10 rounds, 50% of customer joining each round, first in line is always served
+    for (int i = 0; i < 10; i++)
     {
-        cout << current->name << " ordered: " << current->order << endl;
-        current = current->next;
+        //if customer joins
+        if (rand() % 100 < 50)
+        {
+            //create new node
+            coffeeOrder* newNode = new coffeeOrder;
+            newNode->name = names[rand() % 8];
+            newNode->order = drinks[rand() % 5];
+            newNode->next = nullptr;
+            current->next = newNode;
+            current = newNode;
+            //print
+            cout << newNode->name << " joined the queue." << endl;
+        }
+
+        //serve head
+        coffeeOrder* temp = head;
+        head = head->next;
+        cout << temp->name << " ordered: " << temp->order << endl;
+        delete temp;
+
+        //print queue
+        cout << "Queue: ";
+        current = head;
+        while (current != nullptr)
+        {
+            cout << current->name << " ";
+            current = current->next;
+        }
+        cout << endl;
     }
+
     return 0;
 }
